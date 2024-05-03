f804ff4053f73cf82bc05db8a288f28d21772f2c
Alessio Vulpinari <alessiovulpinari99@gmail.com>
2024-05-03 20:46:19 +0200

    Added FiveDaysFetch

--- src/components/WeatherPage.jsx
+++ src/components/WeatherPage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { Alert, Col, Container, Row, Spinner } from "react-bootstrap"
+import { Alert, Col, Container, Row } from "react-bootstrap"
 import { useParams } from "react-router-dom"
 
 const WeatherPage = (props) => {
@@ -7,9 +7,10 @@ const WeatherPage = (props) => {
 
   const [isError, setIsError] = useState(false)
   const [errorMsg, setErrorMsg] = useState("")
-  const [isLoading, setIsLoading] = useState(true)
+  //   const [isLoading, setIsLoading] = useState(true)
   const [todayWeather, setTodayWeather] = useState(null)
-
+  const [fiveDayWeather, setFiveDayWeather] = useState(null)
+  const today = new Date()
   let cityCordinates = null
 
   const fetchCordinates = () => {
@@ -62,6 +63,7 @@ const WeatherPage = (props) => {
           const cordinates = { lat: parseFloat(data[0].lat).toFixed(2), lon: parseFloat(data[0].lon).toFixed(2) }
           cityCordinates = cordinates
           fetchTodayWeather()
+          fetchFiveDaysWeather()
         }
       })
       .catch((err) => {
@@ -130,10 +132,67 @@ const WeatherPage = (props) => {
         setErrorMsg(err.name + " : " + err.message)
         setIsError(true)
       })
-      .finally(() => {
-        // In ogni caso (sia che abbiamo ricevuto un errore o meno) settiamo
-        // isLoading che di default è true a false per disattivare lo spinner
-        setIsLoading(false)
+  }
+
+  const fetchFiveDaysWeather = () => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${cityCordinates.lat}&lon=${cityCordinates.lon}&appid=47682873ae7cd15430297705dd2c8022`
+    )
+      .then((response) => {
+        if (response.ok) {
+          return response.json()
+        } else {
+          if (response.status === 400) {
+            throw new Error("400: Bad Request")
+          }
+          if (response.status === 401) {
+            throw new Error("401: Unauthorized")
+          }
+          if (response.status === 402) {
+            throw new Error("402: Payment Required")
+          }
+          if (response.status === 403) {
+            throw new Error("403: Forbidden")
+          }
+          if (response.status === 404) {
+            throw new Error("404: Not Found")
+          }
+          if (response.status === 405) {
+            throw new Error("405: Method Not Allowed")
+          }
+          if (response.status === 406) {
+            throw new Error("406: Not Acceptable")
+          }
+          if (response.status === 407) {
+            throw new Error("407: Proxy Authentication Required")
+          }
+          if (response.status === 408) {
+            throw new Error("408: Request Timeout")
+          }
+          if (response.status === 500) {
+            throw new Error("500: Server Error")
+          }
+
+          throw new Error("Generic Fetch Error")
+        }
+      })
+      .then((data) => {
+        if (!data.list) {
+          // Se la ricerca non pruduce nessun risultiamo settiamo lo stato isError = true
+          // e lanciamo un errore per indicare che non è stato trovato il tempo climato della la città in questione
+          setIsError(true)
+          throw new Error("No weather info finded")
+        } else {
+          //   getNewDate(today, data.list)
+
+          setFiveDayWeather(data.list)
+        }
+      })
+      .catch((err) => {
+        // In caso di errore settiamo lo stato isError = true
+        // ed utilizziamo il messaggio di errore ricevuto settandolo allo stato errorMsg
+        setErrorMsg(err.name + " : " + err.message)
+        setIsError(true)
       })
   }
 
@@ -160,32 +219,33 @@ const WeatherPage = (props) => {
 
   return (
     <>
-      {isLoading && !isError ? (
-        // in questo caso istanziamo lo spinner per il caricamento
-        <Spinner animation='border' role='status' variant='light' className='mx-auto'>
-          <span className='visually-hidden'>Loading...</span>
-        </Spinner>
-      ) : (
-        console.log("Ciao")
-      )}
       {isError ? createAlert(errorMsg) : console.log("Nessun errore")}
-      {todayWeather && !isError ? (
-        <Container>
+      {todayWeather && !isError && (
+        <Container className='text-bg-dark'>
           <Container>
             <Row>
               <Col>
+                <img
+                  src={`https://openweathermap.org/img/wn/${todayWeather.weather[0].icon}@2x.png`}
+                  alt={todayWeather.weather[0].description}
+                  className='img-fluid'
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={4}>
                 <Row>
                   <Col xs={12}>{todayWeather.weather[0].description}</Col>
                   <Col xs={12}>{kelvinToCelsius(todayWeather.main.temp)} °C</Col>
                 </Row>
               </Col>
-              <Col>
+              <Col xs={4}>
                 <Row>
                   <Col xs={12}>Wind:</Col>
                   <Col xs={12}>{todayWeather.wind.speed} m/s</Col>
                 </Row>
               </Col>
-              <Col>
+              <Col xs={4}>
                 <Row>
                   <Col xs={12}>Humidity:</Col>
                   <Col xs={12}>{todayWeather.main.humidity}%</Col>
@@ -193,10 +253,29 @@ const WeatherPage = (props) => {
               </Col>
             </Row>
           </Container>
-          <Container></Container>
+          <Container>
+            <Row>
+              {fiveDayWeather &&
+                fiveDayWeather.map((obj, i) => (
+                  <Col xs={12} sm={6} key={"Key" + i}>
+                    <div className='d-flex justify-content-between align-items-center'>
+                      <span>
+                        <img
+                          alt={obj.weather[0].description}
+                          src={`https://openweathermap.org/img/wn/${obj.weather[0].icon}@2x.png`}
+                        ></img>
+                      </span>
+                      <span>{obj.dt_txt}</span>
+                    </div>
+                    <div className='d-flex justify-content-between'>
+                      <span>Max: {kelvinToCelsius(obj.main.temp_max)} °C</span>
+                      <span>Min: {kelvinToCelsius(obj.main.temp_min)} </span>
+                    </div>
+                  </Col>
+                ))}
+            </Row>
+          </Container>
         </Container>
-      ) : (
-        console.log("erroe")
       )}
     </>
   )
